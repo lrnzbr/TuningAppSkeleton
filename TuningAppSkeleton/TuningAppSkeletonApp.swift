7e7141decb775bc29796aa8fa621f3ef05bf96fc
Lorenzo Brown <lrnzbr@gmail.com>
2022-06-22 10:30:04 -0500

    Initial Commit

---
+++ TuningAppSkeleton/TuningAppSkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuningAppSkeletonApp.swift
+//  TuningAppSkeleton
+//
+//  Created by Lorenzo Brown on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TuningAppSkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
